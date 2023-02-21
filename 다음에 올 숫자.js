01428472ea9d87d01f570c91419472feb8195214
brgndy <brgndy96@gmail.com>
2023-02-21 11:49:04 +0900

    msg

--- 다음에 올 숫자.js
+++ 다음에 올 숫자.js
@@ -1,11 +1,17 @@
+// 내가 풀었던 코드
+
+let common = [4, 16, 64, 256];
+
 function solution(common) {
   var answer = 0;
   if (common[2] % common[1] === 0) {
     let num = common[2] / common[1];
-    answer = common[common.length - 1] * num;
+    answer = common.pop() * num;
   } else {
     let num = common[2] - common[1];
-    answer = common[common.length - 1] + num;
+    answer = common.pop() + num;
   }
   return answer;
 }
+
+console.log(solution(common));
