4a1f1c9fa21a7754333a54f86d5c28a0a24a96f5
brgndy <brgndy96@gmail.com>
2023-03-03 22:14:49 +0900

    20230303

--- 연속된 수의 합.js
+++ 연속된 수의 합.js
@@ -1,14 +1,12 @@
-const num = 3;
-const total = 3;
+let num = 4;
+let total = 14;
 
 function solution(num, total) {
   var answer = [];
-  let a = "a";
-  for (let i = 1; i <= num; i++) {
-    a += "a" + i;
+  let arr = [];
+
+  for (let i = 0; i < 5; i++) {
+    arr.push("a");
   }
-
-  return a;
+  return answer;
 }
-
-console.log(solution(num, total));
