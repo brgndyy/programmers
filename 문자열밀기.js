ecec81257cf494ac6703f9e5ea4e99ce403693eb
brgndy <brgndy96@gmail.com>
2023-03-07 21:32:54 +0900

    msg

--- 문자열밀기.js
+++ 문자열밀기.js
@@ -2,21 +2,23 @@ const A = "hello";
 const B = "lohel";
 
 function solution(A, B) {
-  let count = 0;
-
   let splitA = A.split("");
-  let splitB = B.split("");
 
   if (A === B) {
     return 0;
   }
 
   for (let i = 0; i < splitA.length; i++) {
-    const splitWord = splitA.pop(); // p
-    const newWord = splitA.unshift(splitWord);
+    //unshift 자체만으로는 해당 요소의 갯수를 반환함. 함수 실행만 해주어야한다.
+    splitA.unshift(splitA.pop());
+    const newWord = splitA.join("");
 
-    return newWord;
+    if (newWord === B) {
+      return i + 1;
+    }
   }
+
+  return -1;
 }
 
 console.log(solution(A, B));
