0acfd2ef1900b296c3614eae068dc908d8dbc490
brgndy <brgndy96@gmail.com>
2023-03-09 10:50:27 +0900

    msg

--- OX퀴즈.js
+++ OX퀴즈.js
@@ -25,3 +25,18 @@ function solution(quiz) {
 }
 
 console.log(solution(quiz));
+
+// function solution(quiz) {
+//     let answer = [];
+//     quiz.forEach((val) => {
+//         const [x, sign, y, , z] = val.split(" ");
+//         let sum = 0;
+//         if (sign === "+") {
+//             sum = Number(x) + Number(y);
+//         } else {
+//             sum = Number(x) - Number(y);
+//         }
+//         sum === Number(z) ? answer.push("O") : answer.push("X");
+//     });
+//     return answer;
+// }
