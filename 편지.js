255066400fe79690aa7273dee9077fd9fa9d22e8
brgndy <brgndy96@gmail.com>
2023-03-10 22:36:56 +0900

    msg

---
+++ 편지.js
@@ -0,0 +1,13 @@
+const n = 29;
+
+function solution(n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+
+console.log(solution(n));
