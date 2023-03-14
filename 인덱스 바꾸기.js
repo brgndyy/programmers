db177dbbf14d5e9ceed9166ee6cb6faa7e6fbfe7
brgndy <brgndy96@gmail.com>
2023-03-14 20:41:56 +0900

    msg

---
+++ 인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+const my_string = "I love you";
+const num1 = 3;
+const num2 = 6;
+
+function solution(my_string, num1, num2) {
+  var answer = "";
+  return answer;
+}
+console.log(solution(my_string, num1, num2));
