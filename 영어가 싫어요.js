d25d77d98d7a767ab73b918f6cdf29187f9e9f96
brgndy <brgndy96@gmail.com>
2023-03-14 21:14:51 +0900

    msg

---
+++ 영어가 싫어요.js
@@ -0,0 +1,8 @@
+const numbers = "onetwothreefourfivesixseveneightnine";
+
+function solution(numbers) {
+  var answer = numbers.includes("one");
+  return answer;
+}
+
+console.log(solution(numbers));
