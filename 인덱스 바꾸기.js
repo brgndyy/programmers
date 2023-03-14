5a3ffba44cd9d45cb1fe656b6135f7517283d7de
brgndy <brgndy96@gmail.com>
2023-03-14 21:01:48 +0900

    msg

--- 인덱스 바꾸기.js
+++ 인덱스 바꾸기.js
@@ -1,9 +1,21 @@
-const my_string = "I love you";
-const num1 = 3;
-const num2 = 6;
+const my_string = "hello";
+const num1 = 1;
+const num2 = 2;
 
 function solution(my_string, num1, num2) {
-  var answer = "";
-  return answer;
+  const splitWord = my_string.split("");
+  const num1Word = splitWord[num1];
+  const num2Word = splitWord[num2];
+
+  splitWord.splice(num1, 1, num2Word);
+  splitWord.splice(num2, 1, num1Word);
+
+  return splitWord.join("");
 }
 console.log(solution(my_string, num1, num2));
+
+// function solution(my_string, num1, num2) {
+//     my_string = my_string.split('');
+//     [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+//     return my_string.join('');
+// }
