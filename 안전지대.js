9ae0989ba3f0c4507e39783231862e774d0ea695
brgndy <brgndy96@gmail.com>
2023-04-03 21:47:46 +0900

    msg

---
+++ 안전지대.js
@@ -0,0 +1,22 @@
+const board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+];
+
+function solution(board) {
+    let BoardNum = 25;
+  var dangerZone = 0;
+
+  if(board[0][0] === 1 || board[0][4] === 1 || board[4][0] ===1 || board[4][4] === 1){
+    
+  }
+  
+
+
+  return answer;
+}
+
+console.log(solution(board));
---
+++ 영어가 싫어요.js
@@ -0,0 +1,20 @@
+const numbers = "onetwothreefourfivesixseveneightnine";
+
+function solution(numbers) {
+  var answer = 0;
+  let oneToTen = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  return answer;
+}
+
+console.log(solution(numbers));
