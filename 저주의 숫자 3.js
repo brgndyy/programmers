b29075478f560cdea3fe54bd4fcf7a627a70ac87
brgndy <brgndy96@gmail.com>
2023-04-07 11:15:32 +0900

    msg

---
+++ 저주의 숫자 3.js
@@ -0,0 +1,8 @@
+const n = 15;
+
+function solution(n) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution(n));
--- 평행.js
+++ 평행.js
@@ -6,25 +6,7 @@ const dots = [
 ];
 
 function solution(dots) {
-  //1번째 케이스
-  let answer = 0;
-
-  function caculateParallel(a, b, c, d) {
-    let abDiff = (b[1] - a[1]) / (b[0] - a[0]);
-    let cdDiff = (d[1] - c[1]) / (d[0] - c[0]);
-
-    if (abDiff === cdDiff) {
-      answer++;
-    }
-
-    return answer;
-  }
-
-  caculateParallel(dots[0], dots[1], dots[2], dots[3]);
-  caculateParallel(dots[0], dots[2], dots[1], dots[3]);
-  caculateParallel(dots[0], dots[3], dots[1], dots[2]);
-
-  return answer > 0 ? 1 : 0;
+  var answer = 0;
 }
 
 console.log(solution(dots));
