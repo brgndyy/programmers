41db73e12d8c89b67b180657c7be5438e72e46d1
brgndy <brgndy96@gmail.com>
2023-04-09 22:26:04 +0900

    msg

---
+++ 캐릭터의 좌표.js
@@ -0,0 +1,13 @@
+const keyinput = ["down", "down", "down", "down", "down"];
+const board = [7, 9];
+
+function solution(keyinput, board) {
+  var answer = [];
+
+  let centerPosition = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
+  return centerPosition;
+}
+
+console.log(solution(keyinput, board));
+
+// 주어진 board의 중앙점을 구해서 이것을 [0,0]이라고 가정하자.
