b8d3f95407cd6d660e842169cd5c5657bb0ccd2d
brgndy <brgndy96@gmail.com>
2023-04-11 15:07:28 +0900

    msg

--- debugging.js
+++ debugging.js
@@ -1,15 +1,39 @@
-const n = 7;
+const n = 15;
 
 function solution(n) {
-  var answer = 0;
-  let factorial = 1;
-
-  while (factorial <= n) {
-    answer++;
-    factorial = factorial * answer;
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    isMixNumber(i) ? count++ : null;
   }
+  return count;
+}
 
-  return answer - 1;
+// 합성수인지 판별하는 함수
+function isMixNumber(num) {
+  let count = 0;
+  // 약수의 개수를 셈
+  for (let i = 1; i <= Math.floor(num / 2); i++) {
+    num % i === 0 ? count++ : null;
+  }
+  return count >= 2 ? true : false;
 }
 
 console.log(solution(n));
+
+function solution(n) {
+  let count = 0;
+  // 1부터 n까지 반복문을 돌리는데, 약수의 갯수를 세는 함수를 실행시킨다.
+  for (let i = 1; i <= n; i++) {
+    isMixNumber(i) ? count++ : null;
+  }
+  return count;
+}
+
+function isMixNumber(number) {
+  let count = 0;
+  for (let i = 1; i < Math.floor(number / 2); i++) {
+    number % i === 0 ? count++ : null;
+  }
+
+  return count >= 2 ? true : false;
+}
