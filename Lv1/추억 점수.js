4ba94b0a22005ac5e16862f99c3e2ea21be5cbfd
brgndy <brgndy96@gmail.com>
2023-04-13 08:54:22 +0900

    msg

--- Lv1/추억 점수.js
+++ Lv1/추억 점수.js
@@ -3,7 +3,22 @@ const yearning = [5, 10, 1, 3];
 const photo = [["may"], ["kein", "deny", "may"], ["kon", "coni"]];
 
 function solution(name, yearning, photo) {
-  var answer = [];
+
+    let pointArr = [];
+    const combined = name.reduce((arr, cur, index))
+
+
+  for(let i = 0 ; i < photo.length; i++){
+    for(let j = 0; j < photo[i].length; j++){
+        let count = 0;
+        if(photo[i][j] === 'may'){
+            count+=5;
+        } else if(photo[i][j] === 'kein'){
+            count+=10;
+        } else if(phto)
+    }
+  }
+
   return answer;
 }
 
