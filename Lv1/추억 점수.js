eacbc000a2bf0cf41ff82d1787f8a0e0b991fc70
brgndy <brgndy96@gmail.com>
2023-04-13 09:20:00 +0900

    msg

--- Lv1/추억 점수.js
+++ Lv1/추억 점수.js
@@ -3,20 +3,24 @@ const yearning = [5, 10, 1, 3];
 const photo = [["may"], ["kein", "deny", "may"], ["kon", "coni"]];
 
 function solution(name, yearning, photo) {
+  let answer = [];
+  let map = new Map();
 
-    let pointArr = [];
-    const combined = name.reduce((arr, cur, index))
+  for (let i = 0; i < name.length; i++) {
+    map.set(name[i], yearning[i]);
+  }
 
+  for (let i = 0; i < photo.length; i++) {
+    let count = 0;
+    for (let j = 0; j < photo[i].length; j++) {
+      const score = map.get(photo[i][j]);
 
-  for(let i = 0 ; i < photo.length; i++){
-    for(let j = 0; j < photo[i].length; j++){
-        let count = 0;
-        if(photo[i][j] === 'may'){
-            count+=5;
-        } else if(photo[i][j] === 'kein'){
-            count+=10;
-        } else if(phto)
+      if (!score) {
+        continue;
+      }
+      count += score;
     }
+    answer.push(count);
   }
 
   return answer;
