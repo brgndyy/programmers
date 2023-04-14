bab8b450fbf1eb9c0c641e5b41bda00738061ee1
brgndy <brgndy96@gmail.com>
2023-04-14 20:53:09 +0900

    msg

--- Lv1/추억 점수.js
+++ Lv1/추억 점수.js
@@ -3,8 +3,6 @@ const yearning = [5, 10, 1, 3];
 const photo = [["may"], ["kein", "deny", "may"], ["kon", "coni"]];
 
 function solution(name, yearning, photo) {
-  let answer = [];
-
   return answer;
 }
 
