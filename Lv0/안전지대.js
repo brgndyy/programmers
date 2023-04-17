c0c0156bf6c828933ac943462b5a6e27e18696a3
brgndy <brgndy96@gmail.com>
2023-04-18 08:53:49 +0900

    msg

--- Lv0/안전지대.js
+++ Lv0/안전지대.js
@@ -6,6 +6,25 @@ let board = [
   [0, 0, 0, 0, 0],
 ];
 
-function solution(board) {}
+function solution(board) {
+  let board = 25;
+
+  const dx = [1, 0, -1, 0, 1, 1, -1, -1];
+  const dy = [0, 1, 0, -1, -1, 1, 1, -1];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 1) {
+        for (let k = 0; k < dx.length; k++) {
+          let cx = i + dx[k]; // 3
+          let cy = j + dy[k]; // 2
+          board[(cx, cy)] += 1;
+        }
+      }
+    }
+  }
+
+  return board;
+}
 
 console.log(solution(board));
