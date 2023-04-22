137e954d9f50b86bc8476882960b2c6aba26c13d
brgndy <brgndy96@gmail.com>
2023-04-22 18:40:48 +0900

    mnsg

--- Lv1/삼총사.js
+++ Lv1/삼총사.js
@@ -5,7 +5,7 @@ function solution(number) {
 
   for (let i = 0; i < number.length - 2; i++) {
     for (let j = i + 1; j < number.length - 1; j++) {
-      for (let k = j + 1; k < number.length; k++) {
+      for (let k = i + 2; k < number.length; k++) {
         if (number[i] + number[j] + number[k] === 0) {
           answer += 1;
         }
--- Lv1/신고 결과 받기.js
+++ Lv1/신고 결과 받기.js
@@ -3,55 +3,20 @@ const report = ["ryan con", "ryan con", "ryan con", "ryan con"];
 const k = 3;
 
 function solution(id_list, report, k) {
-  var answer = [];
-  let accountSus = [];
-
-  let userObj = {};
-  let reportedUserObj = {};
-
-  // 신고 당한 사람 정보 담을 객체 만들어주기
-
-  // {muzi: 0, frodo: 0, apeach: 0, neo: 0}
-  id_list.forEach((id) => (reportedUserObj[id] = 0));
-  id_list.forEach((id) => (userObj[id] = 0));
-
-  // 신고자 중복 제거
-  const realReport = [...new Set(report)];
-
-  // 신고 받은 사람 카운트 해주기
-  // {muzi: 1, frodo: 2, apeach: 0, neo: 2}
-  realReport.forEach((user) => {
-    let splitUser = user.split(" ");
-    let [reporter, getReported] = [splitUser[0], splitUser[1]];
-
-    reportedUserObj[getReported] += 1;
+  let reports = [...new Set(report)].map((a) => {
+    return a.split(" ");
   });
-
-  // 신고 받아서 정지 된사람들
-
-  for (let x in reportedUserObj) {
-    if (reportedUserObj[x] >= k) {
-      accountSus.push(x);
+  let counts = new Map();
+  for (const bad of reports) {
+    counts.set(bad[1], counts.get(bad[1]) + 1 || 1);
+  }
+  let good = new Map();
+  for (const report of reports) {
+    if (counts.get(report[1]) >= k) {
+      good.set(report[0], good.get(report[0]) + 1 || 1);
     }
   }
-
-  // 현재 accountSus = ['frodo', 'neo'];
-
-  realReport.forEach((user) => {
-    let splitUser = user.split(" ");
-    let [reporter, getReported] = [splitUser[0], splitUser[1]];
-
-    accountSus.forEach((user) => {
-      if (getReported === user) {
-        userObj[reporter] += 1;
-      }
-    });
-  });
-
-  for (let x in userObj) {
-    answer.push(userObj[x]);
-  }
-
+  let answer = id_list.map((a) => good.get(a) || 0);
   return answer;
 }
 
