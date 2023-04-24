f80317afd805ba6c6243e81e34b85bb8a0d8389c
brgndy <brgndy96@gmail.com>
2023-04-24 16:51:47 +0900

    msg

--- Lv1/명예의 전당.js
+++ Lv1/명예의 전당.js
@@ -1,18 +1,19 @@
-const k = 3;
-const score = [10, 100, 20, 150, 1, 100, 200];
+const k = 4;
+const score = [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000];
 
 function solution(k, score) {
   var answer = [];
-  let fameArr = [];
 
   for (let i = 0; i < score.length; i++) {
+    let fameArr = [];
+
     fameArr.push(score[i]); // [0, 300, 40, 300, 20  ]
 
     if (i < k) {
       answer.push(Math.min(...fameArr));
     } else {
-      let sortedFameArr = fameArr.sort((a, b) => b - a);
-      answer.push(sortedFameArr[k - 1]);
+      let sortedFameArr = fameArr.sort((a, b) => b - a).splice(0, 4);
+      answer.push(sortedFameArr[sortedFameArr.length - 1]);
     }
   }
 
