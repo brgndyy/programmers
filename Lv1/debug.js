fec3960b2bc42f60c5e129c7e65062aa3528af28
brgndy <brgndy96@gmail.com>
2023-04-25 22:39:45 +0900

    msg

--- Lv1/debug.js
+++ Lv1/debug.js
@@ -1,65 +1,45 @@
-const today = "2020.01.01";
-const terms = ["Z 3", "D 5"];
-const privacies = [
-  "2019.01.01 D",
-  "2019.11.15 Z",
-  "2019.08.02 D",
-  "2019.07.01 D",
-  "2018.12.28 Z",
-];
+const s = "uakks";
+const skip = "wbqd";
+const index = 5;
 
-function solution(today, terms, privacies) {
-  let nowTodayYear = Number(today.split(".")[0]);
-  let nowTodayMonth = Number(today.split(".")[1]);
-  let nowTodayDay = Number(today.split(".")[2]);
+function solution(s, skip, index) {
+  var answer = "";
 
-  var answer = [];
+  //알파벳 배열 만들어주기
+  const alphabet = Array.from({ length: 26 }, (v, i) =>
+    String.fromCharCode(i + 97)
+  );
 
-  // ['A', '6']
-  // ['B', '12']
-  // ['C', '3']
-  let splitTerms = terms.map((term, index) => term.split(" "));
+  [...s].forEach((str) => {
+    let idx = index;
+    let plusAlphabet;
+    let strIndex = alphabet.indexOf(str); // 0
 
-  [...privacies].forEach((privacy, index) => {
-    let term = privacy.split(" ")[1]; // C
-    let termTime = privacy.split(" ")[0]; // 2022.02.19
+    while (idx > 0) {
+      if (strIndex > 25) {
+        strIndex = 0;
+        plusAlphabet = alphabet[strIndex];
+        if (skip.includes(plusAlphabet)) {
+          continue;
+        } else {
+          idx--;
+        }
+      } else {
+        strIndex += 1;
+        plusAlphabet = alphabet[strIndex];
 
-    let termYear = Number(termTime.split(".")[0]); // 2022
-    let termMonth = Number(termTime.split(".")[1]); // 2
-    let termDay = Number(termTime.split(".")[2]); // 19
-
-    // [ 'C', '3' ]
-    let filteredTerm = splitTerms.filter((splitTerm) => splitTerm[0] === term);
-
-    let filteredTermMonthNum = Number(filteredTerm[0][1]); // 3
-
-    let sumMonth = termMonth + filteredTermMonthNum; // 5
-    let sumDay = termDay - 1; // 18
-
-    if (sumMonth > 12) {
-      termYear += 1; // 2020
-      sumMonth -= 12; // 2
+        if (skip.includes(plusAlphabet)) {
+          continue;
+        } else {
+          idx--;
+        }
+      }
     }
 
-    if (sumDay <= 0) {
-      sumDay = 28; // 1
-      sumMonth -= 1; // 1
-    }
-
-    if (termYear < nowTodayYear) {
-      answer.push(index + 1);
-    } else if (termYear === nowTodayYear && sumMonth < nowTodayMonth) {
-      answer.push(index + 1);
-    } else if (
-      termYear === nowTodayYear &&
-      sumMonth === nowTodayMonth &&
-      sumDay < termDay
-    ) {
-      answer.push(index + 1);
-    }
+    answer += plusAlphabet;
   });
 
   return answer;
 }
 
-console.log(solution(today, terms, privacies));
+console.log(solution(s, skip, index));
--- Lv1/둘만의 암호.js
+++ Lv1/둘만의 암호.js
@@ -4,6 +4,31 @@ const index = 5;
 
 function solution(s, skip, index) {
   var answer = "";
+
+  //알파벳 배열 만들어주기
+  const alphabet = Array.from({ length: 26 }, (v, i) =>
+    String.fromCharCode(i + 97)
+  );
+
+  [...s].forEach((str) => {
+    let plusAlphabet;
+    let strIndex = alphabet.indexOf(str); // 0
+
+    for (let i = strIndex + 1; i <= index; i++) {
+      plusAlphabet = alphabet[i];
+
+      if (skip.includes(plusAlphabet)) {
+        i--;
+      }
+
+      if (i > 25) {
+        i = -1;
+      }
+    }
+
+    answer += plusAlphabet;
+  });
+
   return answer;
 }
 
