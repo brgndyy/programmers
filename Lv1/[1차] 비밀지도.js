0d555bf369a34d0d512fee85d117db814fe74943
brgndy <brgndy96@gmail.com>
2023-05-01 23:04:14 +0900

    msg

--- Lv1/[1차] 비밀지도.js
+++ Lv1/[1차] 비밀지도.js
@@ -3,6 +3,9 @@ const arr1 = [9, 20, 28, 18, 11];
 const arr2 = [30, 1, 21, 17, 28];
 
 function solution(n, arr1, arr2) {
+
+  let arr1Arr = arr1.map(num => )
+
   return arr1Two;
 }
 
--- Lv1/debug.js
+++ Lv1/debug.js
@@ -1,13 +1,39 @@
-const input = `KKHSSSSSSSE`;
+const n = 5;
+const arr1 = [9, 20, 28, 18, 11];
+const arr2 = [30, 1, 21, 17, 28];
 
-let set = [...new Set(input)];
-let splitInput = input.split("");
-let answer = "";
+function solution(n, arr1, arr2) {
+  // arr1 배열 만들어주기
+  let arr1Arr = arr1.map((str) => str.toString(2));
+  let arr2Arr = arr2.map((str) => str.toString(2));
+  let answerArr = [];
 
-for (let i = 0; i < set.length; i++) {
-  let num = splitInput.filter((str) => str === set[i]).length;
+  let sumArr = arr1Arr
+    .map((num, index) => Number(num) + Number(arr2Arr[index]))
+    .map((num) => {
+      if (num.length !== n) {
+        return String(num).padStart(n, "0");
+      } else {
+        return String(num);
+      }
+    });
 
-  num === 1 ? (answer += set[i]) : (answer += set[i] + num);
+  for (let i = 0; i < sumArr.length; i++) {
+    let answer = "";
+
+    let splitSumArr = sumArr[i].split("");
+
+    for (let j = 0; j < splitSumArr.length; j++) {
+      if (splitSumArr[j] === "1" || splitSumArr[j] === "2") {
+        answer += "#";
+      } else {
+        answer += " ";
+      }
+    }
+    answerArr.push(answer);
+  }
+
+  return answerArr;
 }
 
-console.log(answer);
+console.log(solution(n, arr1, arr2));
