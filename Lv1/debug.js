22f7b2622a65caee30c0ba7fc8afef0019b45e51
brgndy <brgndy96@gmail.com>
2023-05-03 09:45:52 +0900

    msg

--- Lv1/debug.js
+++ Lv1/debug.js
@@ -1,39 +1,11 @@
-const n = 5;
-const arr1 = [9, 20, 28, 18, 11];
-const arr2 = [30, 1, 21, 17, 28];
+const X = "100";
+const Y = "123450";
 
-function solution(n, arr1, arr2) {
-  // arr1 배열 만들어주기
-  let arr1Arr = arr1.map((str) => str.toString(2));
-  let arr2Arr = arr2.map((str) => str.toString(2));
-  let answerArr = [];
+function solution(X, Y) {
+  let sortX = X.split("").sort((a, b) => Number(b) - Number(a));
+  let sortY = Y.split("").sort((a, b) => Number(b) - Number(a));
 
-  let sumArr = arr1Arr
-    .map((num, index) => Number(num) + Number(arr2Arr[index]))
-    .map((num) => {
-      if (num.length !== n) {
-        return String(num).padStart(n, "0");
-      } else {
-        return String(num);
-      }
-    });
-
-  for (let i = 0; i < sumArr.length; i++) {
-    let answer = "";
-
-    let splitSumArr = sumArr[i].split("");
-
-    for (let j = 0; j < splitSumArr.length; j++) {
-      if (splitSumArr[j] === "1" || splitSumArr[j] === "2") {
-        answer += "#";
-      } else {
-        answer += " ";
-      }
-    }
-    answerArr.push(answer);
-  }
-
-  return answerArr;
+  return sortY;
 }
 
-console.log(solution(n, arr1, arr2));
+console.log(solution(X, Y));
--- Lv1/숫자 짝꿍.js
+++ Lv1/숫자 짝꿍.js
@@ -1,6 +1,11 @@
 const X = "100";
 const Y = "123450";
 
-function solution(X, Y) {}
+function solution(X, Y) {
+  let sortX = X.split("").sort((a, b) => Number(b) - Number(a));
+  let sortY = Y.split("").sort((a, b) => Number(b) - Number(a));
+
+  return sortY;
+}
 
 console.log(solution(X, Y));
