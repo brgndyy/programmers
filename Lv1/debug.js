2102a693c9b5de8edc6edde50deee4406de88f53
brgndy <brgndy96@gmail.com>
2023-05-03 10:03:10 +0900

    msg

--- Lv1/debug.js
+++ Lv1/debug.js
@@ -4,8 +4,9 @@ const Y = "123450";
 function solution(X, Y) {
   let sortX = X.split("").sort((a, b) => Number(b) - Number(a));
   let sortY = Y.split("").sort((a, b) => Number(b) - Number(a));
+  let filtered = sortX.filter((str) => Y.includes(str));
 
-  return sortY;
+  return filtered;
 }
 
 console.log(solution(X, Y));
