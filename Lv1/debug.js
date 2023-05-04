312a311351c671f7c1b1f1289f42d2b4b2329a0f
brgndy <brgndy96@gmail.com>
2023-05-04 16:39:15 +0900

    msg

--- Lv1/debug.js
+++ Lv1/debug.js
@@ -1,30 +1,28 @@
-const number = 10;
-const limit = 3;
-const power = 2;
+const s = "abracadabra";
 
-function solution(number, limit, power) {
-  let measureArr = [];
+function solution(s) {
+  let splitS = s.split("");
 
-  for (let i = 1; i <= number; i++) {
-    let count = 0;
-    for (let j = 1; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        if (i / j === j) {
-          count++;
-        } else {
-          count += 2;
-        }
-      }
+  let stack = [];
+  let count = 0;
+
+  for (let i = 0; i < splitS.length; i++) {
+    stack.push(splitS[i]);
+
+    let sameWord = stack.filter((str) => str === stack[0]);
+    let differWord = stack.filter((str) => str !== stack[0]);
+
+    if (sameWord.length === differWord.length) {
+      count++;
+      stack = [];
     }
-
-    if (count > limit) {
-      count = power;
-    }
-
-    measureArr.push(count);
   }
 
-  return measureArr.reduce((a, b) => a + b, 0);
+  if (stack.length !== 0) {
+    count++;
+  }
+
+  return count;
 }
 
-console.log(solution(number, limit, power));
+console.log(solution(s));
