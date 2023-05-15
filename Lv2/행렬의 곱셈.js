7b70ad60f404446222087c20f2eba2d9adb2d9ef
brgndy <brgndy96@gmail.com>
2023-05-15 16:57:44 +0900

    Msg

---
+++ Lv2/행렬의 곱셈.js
@@ -0,0 +1,17 @@
+const arr1 = [
+  [2, 3, 2],
+  [4, 2, 4],
+  [3, 1, 4],
+];
+const arr2 = [
+  [5, 4, 3],
+  [2, 4, 1],
+  [3, 1, 1],
+];
+
+function solution(arr1, arr2) {
+  var answer = [[]];
+  return answer;
+}
+
+console.log(solution(arr1, arr2));
