d3a4638ffacd732dd40d9bcdf09de5ffbf1cb33d
brgndy <brgndy96@gmail.com>
2023-05-22 22:00:14 +0900

    msg

--- Lv1/기사단원의 무기.js
+++ Lv1/기사단원의 무기.js
@@ -2,6 +2,30 @@ const number = 10;
 const limit = 3;
 const power = 2;
 
-function solution(number, limit, power) {}
+function solution(number, limit, power) {
+  let knights = Array.from({ length: number }, (_, index) => index + 1);
+
+  let measureArr = [];
+
+  for (let i = 0; i < knights.length; i++) {
+    let count = 0;
+
+    for (let j = 1; j <= Math.ceil(knights[i] / 2); j++) {
+      if (knights[i] % j === 0) {
+        if (knights[i] / j === j) {
+          count += 1;
+        } else {
+          count += 2;
+        }
+      }
+    }
+
+    measureArr.push(count);
+  }
+
+  let filterArr = measureArr.map((num) => (num > limit ? (num = power) : num));
+
+  return filterArr.reduce((a, b) => a + b, 0);
+}
 
 console.log(solution(number, limit, power));
