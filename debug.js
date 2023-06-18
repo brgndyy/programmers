005abd9f639c32b6ae5b1b65f97d7128fa078224
brgndy <brgndy96@gmail.com>
2023-06-18 18:36:52 +0900

    msg

--- debug.js
+++ debug.js
@@ -3,35 +3,25 @@ const s = "abcabcabcabcdededededede";
 function solution(s) {
   let answer = s.length;
 
-  for (let i = 1; i <= parseInt(s.length / 2); i++) {
-    let str = "";
-    let cnt = 1;
-    let tempStr = s.substr(0, i);
+  for (let i = 1; i <= Math.floor(s.length / 2); i++) {
+    let sentence = "";
     let idx = 0;
 
-    for (idx = i; idx <= s.length; idx += i) {
-      let nextStr = s.substr(idx, i);
-
-      if (tempStr === nextStr) {
-        cnt += 1;
-      } else {
-        if (cnt === 1) {
-          str = str + tempStr;
-        } else {
-          str = str + cnt + tempStr;
-        }
-
-        cnt = 1;
-        tempStr = nextStr;
+    while (idx < s.length) {
+      let cnt = 1;
+      while (s.slice(idx, idx + i) === s.slice(idx + i, idx + i + i)) {
+        cnt++;
+        idx += i;
       }
-    }
 
-    if (cnt === 1) {
-      str = str + tempStr;
-    } else {
-      str = str + cnt + tempStr;
+      if (cnt > 1) {
+        sentence += cnt;
+      }
+      const str = s.slice(idx, idx + i);
+      sentence = sentence + str;
+      idx += i;
     }
-    answer = Math.min(answer, str.length);
+    answer = Math.min(answer, sentence.length);
   }
 
   return answer;
