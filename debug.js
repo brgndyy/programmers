c15cb6bf71bc5aa5c95878434d8414d7267511b5
brgndy <brgndy96@gmail.com>
2023-06-18 19:53:56 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,30 +1,62 @@
-const s = "abcabcabcabcdededededede";
+const fees = [180, 5000, 10, 600];
+const records = [
+  "05:34 5961 IN",
+  "06:00 0000 IN",
+  "06:34 0000 OUT",
+  "07:59 5961 OUT",
+  "07:59 0148 IN",
+  "18:59 0000 IN",
+  "19:09 0148 OUT",
+  "22:59 5961 IN",
+  "23:00 5961 OUT",
+];
 
-function solution(s) {
-  let answer = s.length;
+function solution(fees, records) {
+  const carRecords = {};
+  const carFees = {};
 
-  for (let i = 1; i <= Math.floor(s.length / 2); i++) {
-    let sentence = "";
-    let idx = 0;
-
-    while (idx < s.length) {
-      let cnt = 1;
-      while (s.slice(idx, idx + i) === s.slice(idx + i, idx + i + i)) {
-        cnt++;
-        idx += i;
-      }
-
-      if (cnt > 1) {
-        sentence += cnt;
-      }
-      const str = s.slice(idx, idx + i);
-      sentence = sentence + str;
-      idx += i;
+  records.forEach((record) => {
+    const [time, number, status] = record.split(" ");
+    if (!carRecords[number]) {
+      carRecords[number] = [];
     }
-    answer = Math.min(answer, sentence.length);
-  }
+    carRecords[number].push({ time: timeToMinutes(time), status });
+  });
 
-  return answer;
+  Object.keys(carRecords).forEach((carNumber) => {
+    const timeLogs = carRecords[carNumber];
+    let totalTime = 0;
+    let inTime = null;
+
+    timeLogs.forEach((log) => {
+      if (log.status === "IN") {
+        inTime = log.time;
+      } else if (log.status === "OUT") {
+        totalTime += log.time - inTime;
+        inTime = null;
+      }
+    });
+
+    // 차가 아직 출차가 안됐다면
+    if (inTime !== null) {
+      totalTime += 23 * 60 + 59 - inTime;
+    }
+
+    let fee = fees[1];
+    if (totalTime > fees[0]) {
+      fee += Math.ceil((totalTime - fees[0]) / fees[2]) * fees[3];
+    }
+
+    carFees[carNumber] = fee;
+  });
+
+  const sortedCarNumbers = Object.keys(carFees).sort((a, b) => a - b);
+  return sortedCarNumbers.map((number) => carFees[number]);
 }
 
-console.log(solution(s));
+function timeToMinutes(time) {
+  const [hours, minutes] = time.split(":").map(Number);
+  return hours * 60 + minutes;
+}
+
+console.log(solution(fees, records));
