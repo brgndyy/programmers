9f990e646ea34d70e4918dce1339a5f793e724c6
brgndy <brgndy96@gmail.com>
2023-06-19 10:13:53 +0900

    msg

--- Lv1/공원 산책.js
+++ Lv1/공원 산책.js
@@ -2,49 +2,127 @@ const park = ["OSO", "OOO", "OXO", "OOO"];
 const routes = ["E 2", "S 3", "W 1"];
 
 function solution(park, routes) {
-  // 시작점 구하기
   let answer = [];
-  let newPark = park.map((arr) => arr.split(""));
-  let wLength = newPark[0].length;
-  let hLength = newPark.length;
-  let startPos = []; // [0, 1];
-  newPark.forEach((pos, index) => {
-    if (pos.includes("S")) {
-      startPos.push(index, pos.indexOf("S"));
+  let map = park.map((dir) => dir.split(""));
+  let startPos = [];
+
+  let dir = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+
+  // 시작점 구하기
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] === "S") {
+        startPos.push(i, j);
+        break;
+      }
     }
-  });
+  }
+  // 시작점 = [0, 1];
 
-  let direction = {
-    E: [0, 1],
-    W: [0, -1],
-    S: [1, 0],
-    N: [-1, 0],
-  };
+  for (let i = 0; i < routes.length; i++) {
+    let [direction, dirNum] = [
+      routes[i].split(" ")[0],
+      Number(routes[i].split(" ")[1]),
+    ];
+    let count = 0;
 
-  let movePos = [...routes].map((route) => route.split(" "));
+    if (direction === "E") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[1][0];
+        let ny = startPos[1] + dir[1][1];
 
-  for (let i = 0; i < movePos.length; i++) {
-    let direc = movePos[i][0];
-    let direcNum = Number(movePos[i][1]);
-    let [nx, ny] = startPos;
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0];
+          startPos[1] = startPos[1] + count * -1;
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "S") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[2][0];
+        let ny = startPos[1] + dir[2][1];
 
-    for (let j = 1; j <= direcNum; j++) {
-      nx += direction[direc][0];
-      ny += direction[direc][0];
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0] + count * -1;
+          startPos[1] = startPos[1];
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "W") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[3][0];
+        let ny = startPos[1] + dir[3][1];
 
-      if (
-        nx >= 0 &&
-        ny >= 0 &&
-        nx < wLength &&
-        ny < hLength &&
-        newPark[nx][ny] !== "X"
-      ) {
-        answer = [nx, ny];
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0];
+          startPos[1] = startPos[1] + count;
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "N") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[0][0];
+        let ny = startPos[1] + dir[0][1];
+
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0] + count;
+          startPos[1] = startPos[1];
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
       }
     }
   }
 
-  return answer;
+  return startPos;
 }
 
 console.log(solution(park, routes));
--- debug.js
+++ debug.js
@@ -1,62 +1,128 @@
-const fees = [180, 5000, 10, 600];
-const records = [
-  "05:34 5961 IN",
-  "06:00 0000 IN",
-  "06:34 0000 OUT",
-  "07:59 5961 OUT",
-  "07:59 0148 IN",
-  "18:59 0000 IN",
-  "19:09 0148 OUT",
-  "22:59 5961 IN",
-  "23:00 5961 OUT",
-];
+const park = ["OSO", "OOO", "OXO", "OOO"];
+const routes = ["E 2", "S 3", "W 1"];
 
-function solution(fees, records) {
-  const carRecords = {};
-  const carFees = {};
+function solution(park, routes) {
+  let answer = [];
+  let map = park.map((dir) => dir.split(""));
+  let startPos = [];
 
-  records.forEach((record) => {
-    const [time, number, status] = record.split(" ");
-    if (!carRecords[number]) {
-      carRecords[number] = [];
-    }
-    carRecords[number].push({ time: timeToMinutes(time), status });
-  });
+  let dir = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
 
-  Object.keys(carRecords).forEach((carNumber) => {
-    const timeLogs = carRecords[carNumber];
-    let totalTime = 0;
-    let inTime = null;
-
-    timeLogs.forEach((log) => {
-      if (log.status === "IN") {
-        inTime = log.time;
-      } else if (log.status === "OUT") {
-        totalTime += log.time - inTime;
-        inTime = null;
+  // 시작점 구하기
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] === "S") {
+        startPos.push(i, j);
+        break;
       }
-    });
-
-    // 차가 아직 출차가 안됐다면
-    if (inTime !== null) {
-      totalTime += 23 * 60 + 59 - inTime;
     }
+  }
+  // 시작점 = [0, 1];
 
-    let fee = fees[1];
-    if (totalTime > fees[0]) {
-      fee += Math.ceil((totalTime - fees[0]) / fees[2]) * fees[3];
+  for (let i = 0; i < routes.length; i++) {
+    let [direction, dirNum] = [
+      routes[i].split(" ")[0],
+      Number(routes[i].split(" ")[1]),
+    ];
+    let count = 0;
+
+    if (direction === "E") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[1][0];
+        let ny = startPos[1] + dir[1][1];
+
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0];
+          startPos[1] = startPos[1] + count * -1;
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "S") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[2][0];
+        let ny = startPos[1] + dir[2][1];
+
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0] + count * -1;
+          startPos[1] = startPos[1];
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "W") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[3][0];
+        let ny = startPos[1] + dir[3][1];
+
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0];
+          startPos[1] = startPos[1] + count;
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
+    } else if (direction === "N") {
+      while (dirNum !== 0) {
+        let nx = startPos[0] + dir[0][0];
+        let ny = startPos[1] + dir[0][1];
+
+        if (
+          nx < 0 ||
+          ny < 0 ||
+          nx >= map.length ||
+          ny >= map[0].length ||
+          map[nx][ny] === "X"
+        ) {
+          startPos[0] = startPos[0] + count;
+          startPos[1] = startPos[1];
+          count = 0;
+          break;
+        } else {
+          startPos = [nx, ny];
+          count++;
+          dirNum--;
+        }
+      }
     }
+  }
 
-    carFees[carNumber] = fee;
-  });
-
-  const sortedCarNumbers = Object.keys(carFees).sort((a, b) => a - b);
-  return sortedCarNumbers.map((number) => carFees[number]);
+  return startPos;
 }
 
-function timeToMinutes(time) {
-  const [hours, minutes] = time.split(":").map(Number);
-  return hours * 60 + minutes;
-}
-
-console.log(solution(fees, records));
+console.log(solution(park, routes));
