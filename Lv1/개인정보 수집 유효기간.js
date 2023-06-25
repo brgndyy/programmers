f30fff52f75cbe3d461fb300fdcb39eacedad8b8
brgndy <brgndy96@gmail.com>
2023-06-25 13:53:35 +0900

    msg

--- Lv1/개인정보 수집 유효기간.js
+++ Lv1/개인정보 수집 유효기간.js
@@ -1,38 +1,54 @@
-const today = "2022.05.19";
-const terms = ["A 6", "B 12", "C 3"];
-const privacies = [
-  "2021.07.01 A",
-  "2021.07.01 B",
-  "2022.02.19 C",
-  "2022.02.20 C",
-];
-
 function solution(today, terms, privacies) {
-  let todayDate = new Date(today);
   let answer = [];
 
-  let newTerms = terms.map((arr) => {
-    let type = arr.split(" ")[0];
-    let num = Number(arr.split(" ")[1]);
+  // 1년에 336일
+  let splitToday = today.split(".");
 
-    return [type, num];
-  });
+  // 현재 총 날짜
+  let totalToday = 0;
 
-  privacies.forEach((str, index) => {
-    let date = str.split(" ")[0];
-    let privacyDate = new Date(date);
-    let type = str.split(" ")[1];
-
-    let termPeroid = newTerms.filter((arr) => arr[0] === type)[1]; //6
-
-    privacyDate.setMonth(privacyDate.getMonth() + termPeroid);
-
-    if (privacyDate <= todayDate) {
-      answer.push(index + 1);
+  // 현재 총 날짜 구하기
+  for (let i = 0; i < splitToday.length; i++) {
+    if (i === 0) {
+      totalToday += Number(splitToday[i]) * 336;
+    } else if (i === 1) {
+      totalToday += Number(splitToday[i]) * 28;
+    } else if (i === 2) {
+      totalToday += Number(splitToday[i]);
     }
-  });
+  }
+
+  let termObj = {};
+
+  // 개인정보 약관 마다 유효기관 객체로 보관
+  for (let i = 0; i < terms.length; i++) {
+    let [type, num] = [terms[i].split(" ")[0], Number(terms[i].split(" ")[1])];
+
+    let totalDay = num * 28;
+    termObj[type] = totalDay;
+  }
+
+  // 개인정보 수집 일자 반복문 돌기
+
+  for (let i = 0; i < privacies.length; i++) {
+    let totalDay = 0;
+    let [yearAndMonthAndDay, type] = privacies[i].split(" ");
+
+    let [year, month, day] = yearAndMonthAndDay
+      .split(".")
+      .map((str) => Number(str));
+
+    totalDay += year * 336;
+    totalDay += month * 28;
+    totalDay += day;
+    totalDay += termObj[type];
+
+    // 현재 총 day는 678749
+    // 총 날짜에서 -1을 해주어야지 보관 가능한 날짜를 구함
+    if (totalToday > totalDay - 1) {
+      answer.push(i + 1);
+    }
+  }
 
   return answer;
 }
-
-console.log(solution(today, terms, privacies));
