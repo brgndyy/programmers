d2e40f2a1328e95bf7a57664211b07a8c8bcb61c
brgndy <brgndy96@gmail.com>
2023-06-25 13:24:45 +0900

    msg

--- Lv1/문자열 나누기.js
+++ Lv1/문자열 나누기.js
@@ -1,30 +1,42 @@
-const s = "aaabbaccccabba";
-
 function solution(s) {
-  let sameStack = [];
-  let diffStack = [];
   let count = 0;
+
   let splitS = s.split("");
 
-  sameStack.push(splitS[0]);
+  let firstStrMap = new Map();
+  let differentMap = new Map();
 
-  for (let i = 1; i < splitS.length; i++) {
-    if (sameStack.length === 0 && diffStack.length === 0) {
-      sameStack.push(splitS[i]);
-    } else if (sameStack[sameStack.length - 1] === splitS[i]) {
-      sameStack.push(splitS[i]);
+  for (let i = 0; i < splitS.length; i++) {
+    // 첫글자 아무것도 없을때
+    if (firstStrMap.size === 0) {
+      firstStrMap.set(splitS[i], 1);
+    }
+    // 첫글자가 이미 존재했을때
+    else if (firstStrMap.get(splitS[i])) {
+      firstStrMap.set(splitS[i], firstStrMap.get(splitS[i]) + 1);
     } else {
-      diffStack.push(splitS[i]);
+      differentMap.set(
+        "differentStr",
+        differentMap.get("differentStr") + 1 || 1
+      );
     }
 
-    if (sameStack.length === diffStack.length) {
+    let firstStrArr = [...firstStrMap];
+    let differentStrArr = [...differentMap];
+
+    if (
+      firstStrArr.length > 0 &&
+      differentStrArr.length > 0 &&
+      firstStrArr[0][1] === differentStrArr[0][1]
+    ) {
+      firstStrMap.clear();
+      differentMap.clear();
       count++;
-      sameStack = [];
-      diffStack = [];
     }
   }
 
+  // 아직 남은 문자가 있다면
+  count += firstStrMap.size;
+
   return count;
 }
-
-console.log(solution(s));
