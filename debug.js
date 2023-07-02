d0882a3bcb5d133d443a5502acf1b6cf887b8655
brgndy <brgndy96@gmail.com>
2023-07-02 10:26:05 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,36 +1,26 @@
 const n = 3;
-const k = 5;
 
-function solution(n, k) {
-  let arr = Array.from({ length: n }, (_, index) => index + 1);
-  let answer = [];
+function solution(n) {
+  const answer = [];
 
-  function factorial(n) {
-    let res = 1;
-    for (let i = 2; i <= n; i++) {
-      res *= i;
-    }
-    return res;
-  }
-
-  let nth = k - 1;
-
-  while (arr.length) {
-    if (nth === 0) {
-      answer.push(...arr);
-      break;
+  function hanoi(n, from, to, by) {
+    if (n === 1) {
+      answer.push([from, to]);
     }
 
-    const fact = factorial(arr.length - 1);
-    const index = (nth / fact) >> 0;
-
-    nth = nth % fact;
-
-    answer.push(arr[index]);
-    arr.splice(index, 1);
+    hanoi(n - 1, from, by, to);
+    answer.push([from, to]);
+    hanoi(n - 1, by, to, from);
   }
+
+  hanoi(n, 1, 3, 2);
 
   return answer;
 }
 
-console.log(solution(n, k));
+console.log(solution(n));
+
+// hanoi(2, 1, 3, 2);
+// hano(1, 1, 2, 3) => n이 1이기 때문에 answer.push([1, 2]) 후 스택에서 사리짐
+// 그 후 1번 주석 라인에 있던 hanoi(2, 1, 3, 2)의 1 3 이 answer.push([1, 3]) 됨
+// 13번라인 hanoi 함수가 실행되어 hanoi(1, 2, 3, 1)이 실행되고 n이 1이므로 answer에 [2, 3]이 push
