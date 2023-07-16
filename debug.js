1188ead55182265979b27e882091e65969ee3476
brgndy <brgndy96@gmail.com>
2023-07-16 11:19:04 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,6 +1,30 @@
-const str1 = "sing";
-const str2 = "sting";
+const board = [
+  [0, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [0, 0, 1, 0],
+];
 
-function isSubsequence(str1, str2) {}
+function solution(board) {
+  let answer = 0;
 
-console.log(isSubsequence(str1, str2));
+  let n = board.length;
+  let m = board[0].length;
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      if (board[i][j] > 0) {
+        let minNum =
+          Math.min(board[i - 1][j], board[i - 1][j - 1], board[i][j - 1]) + 1;
+
+        board[i][j] = minNum;
+
+        answer = Math.max(answer, board[i][j]);
+      }
+    }
+  }
+
+  return board;
+}
+
+console.log(solution(board));
