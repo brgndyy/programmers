ef181683c220e1cb055ddb062c57ac02e4f225a3
brgndy <brgndy96@gmail.com>
2023-07-18 09:06:12 +0900

    msg

--- Lv2/땅따먹기.js
+++ Lv2/땅따먹기.js
@@ -4,22 +4,6 @@ const land = [
   [4, 3, 2, 1],
 ];
 
-function solution(land) {
-  let maxNum = 0;
-
-  let N = land.length;
-
-  for (let i = 1; i < N; i++) {
-    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
-
-    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
-
-    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
-
-    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
-  }
-
-  return Math.max(...land[land.length - 1]);
-}
+function solution(land) {}
 
 console.log(solution(land));
