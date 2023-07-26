de756e8cd625653231ea343565ce34996c1a75f0
brgndy <brgndy96@gmail.com>
2023-07-26 09:42:52 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,37 +1,12 @@
-const arr1 = [
-  [1, 4],
-  [3, 2],
-  [4, 1],
-];
-const arr2 = [
-  [3, 3],
-  [3, 3],
-];
-function solution(arr1, arr2) {
-  let answer = [];
-
-  for (let i = 0; i < arr1.length; i++) {
-    let numArr = [];
-    for (let j = 0; j < arr2[0].length; j++) {
-      let sum = 0;
-
-      for (let m = 0; m < arr2.length; m++) {
-        sum += arr1[i][m] * arr2[m][j];
-      }
-
-      numArr.push(sum);
-    }
-
-    answer.push(numArr);
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  if (num <= 1) {
+    return 1;
   }
 
-  return answer;
+  return num * factorial(num - 1);
 }
 
-console.log(solution(arr1, arr2));
-
-00;
-10;
-
-01;
-11;
+console.log(factorial(7));
