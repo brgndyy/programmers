8cd19cd6c5130ffcc8a1da54d7990f879451373d
brgndy <brgndy96@gmail.com>
2023-07-31 09:42:52 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,5 +1,22 @@
 function binarySearch(arr, targetNum) {
+  let rowIdx = 0;
+  let highIdx = arr.length - 1;
+
   let middleIdx = Math.floor(arr.length / 2);
 
-  for (let i = 0; i < arr.length; i++) {}
+  while (rowIdx <= highIdx) {
+    let middleIdx = Math.floor((rowIdx + highIdx) / 2);
+
+    if (arr[middleIdx] === targetNum) {
+      return middleIdx;
+    } else if (arr[middleIdx] > targetNum) {
+      highIdx = middleIdx - 1;
+    } else if (arr[middleIdx] < targetNum) {
+      rowIdx = middleIdx + 1;
+    }
+  }
+
+  return -1;
 }
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
