4172b68c42024760b29844ad3ef56ca457cc18cf
brgndy <brgndy96@gmail.com>
2023-08-02 08:19:37 +0900

    Msg

--- debug.js
+++ debug.js
@@ -1,22 +1,57 @@
-function binarySearch(arr, targetNum) {
-  let rowIdx = 0;
-  let highIdx = arr.length - 1;
+const m = 4;
+const n = 5;
+const board = ["CCBDE", "AAADE", "AAABF", "CCBBF"];
 
-  let middleIdx = Math.floor(arr.length / 2);
+function solution(m, n, board) {
+  let gameBoard = board.map((str) => str.split(""));
 
-  while (rowIdx <= highIdx) {
-    let middleIdx = Math.floor((rowIdx + highIdx) / 2);
+  let answer = 0;
 
-    if (arr[middleIdx] === targetNum) {
-      return middleIdx;
-    } else if (arr[middleIdx] > targetNum) {
-      highIdx = middleIdx - 1;
-    } else if (arr[middleIdx] < targetNum) {
-      rowIdx = middleIdx + 1;
+  while (true) {
+    let founded = [];
+
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        if (
+          gameBoard[i][j] !== 0 &&
+          gameBoard[i][j] === gameBoard[i][j + 1] &&
+          gameBoard[i][j] === gameBoard[i + 1][j] &&
+          gameBoard[i][j] === gameBoard[i + 1][j + 1]
+        ) {
+          founded.push([i, j]);
+        }
+      }
+    }
+
+    if (!founded.length) {
+      break;
+    }
+
+    founded.forEach((arr) => {
+      gameBoard[arr[0]][arr[1]] = 0;
+      gameBoard[arr[0]][arr[1] + 1] = 0;
+      gameBoard[arr[0] + 1][arr[1]] = 0;
+      gameBoard[arr[0] + 1][arr[1] + 1] = 0;
+
+      answer += 4;
+    });
+
+    for (let i = 0; i < n; i++) {
+      for (let j = m - 1; j >= 0; j--) {
+        if (gameBoard[j][i] === 0) {
+          for (let k = j - 1; k >= 0; k--) {
+            if (gameBoard[k][i] !== 0) {
+              gameBoard[j][i] = gameBoard[k][i];
+              gameBoard[k][i] = 0;
+              break;
+            }
+          }
+        }
+      }
     }
   }
 
-  return -1;
+  return answer;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 5));
+console.log(solution(m, n, board));
