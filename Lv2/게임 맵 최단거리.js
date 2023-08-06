8af761a28fc007dd86acca70aa5c663d125e1100
brgndy <brgndy96@gmail.com>
2023-08-06 12:19:28 +0900

    msg

--- Lv2/게임 맵 최단거리.js
+++ Lv2/게임 맵 최단거리.js
@@ -7,9 +7,6 @@ const maps = [
 ];
 
 function solution(maps) {
-  // 시작점, 움직인 횟수
-  let movePos = [0, 0, 0];
-
   let dir = [
     [-1, 0],
     [0, 1],
@@ -17,15 +14,14 @@ function solution(maps) {
     [0, -1],
   ];
 
-  let queue = [];
-
-  queue.push(movePos);
+  let queue = [[0, 0, 1]];
 
   while (queue.length) {
     let [x, y, move] = queue.shift();
 
-    if (x === 4 && y === 4) {
+    if (x === n - 1 && y === m - 1) {
       return move;
+      break;
     }
 
     for (let i = 0; i < 4; i++) {
@@ -33,7 +29,11 @@ function solution(maps) {
       let ny = y + dir[i][1];
 
       if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5 && maps[nx][ny] !== 0) {
-        queue.push([nx, ny, move++]);
+        // move += 1 이런식으로 하면 반복문을 돌면서 move 값이 누적되어서
+        // 값이 정답보다 더 많이 나올수도 있음
+        queue.push([nx, ny, move + 1]);
+
+        //방문한 처리를 해주어서 효율성을 높여야함
         maps[nx][ny] = 0;
       }
     }
