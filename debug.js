9cd00d519054284e3d2f8816edf2e9a769629ad8
brgndy <brgndy96@gmail.com>
2023-08-25 14:09:25 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,38 +1,7 @@
-const n = 5;
-const k = 110;
+const n = 2;
 
-function solution(n, k) {
-  let arr = Array.from({ length: n }, (_, index) => index + 1);
-  let answer = [];
-
-  let targetInedx = k - 1;
-
-  while (arr.length) {
-    if (targetInedx === 0) {
-      answer.push(...arr);
-      break;
-    }
-
-    let period = factorial(n - 1);
-
-    let groupIndex = Math.floor(targetInedx / period);
-
-    targetInedx = targetInedx % period;
-
-    answer.push(arr[groupIndex]);
-
-    arr.splice(groupIndex, 1);
-  }
-
-  function factorial(n) {
-    if (n === 1) {
-      return 1;
-    }
-
-    return n * factorial(n - 1);
-  }
-
-  return answer;
+function solution(n) {
+  let answer = [[]];
 }
 
-console.log(solution(n, k));
+console.log(solution(n));
