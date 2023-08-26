0306ac6773360446fb4903b4daf93617560fd75b
brgndy <brgndy96@gmail.com>
2023-08-26 14:16:12 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,7 +1,24 @@
 const n = 2;
 
 function solution(n) {
-  let answer = [[]];
+  let answer = [];
+
+  function hanoi(n, from, to, by) {
+    if (n === 1) {
+      answer.push([from, to]);
+      return;
+    }
+
+    hanoi(n - 1, from, by, to);
+
+    answer.push([from, to]);
+
+    hanoi(n - 1, by, to, from);
+  }
+
+  hanoi(n, 1, 3, 2);
+
+  return answer;
 }
 
 console.log(solution(n));
