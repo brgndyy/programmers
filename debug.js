5a67566a024952e94f86b3ffcb0c620fe745b7fc
brgndy <brgndy96@gmail.com>
2023-08-31 08:26:32 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,25 +1,53 @@
-const numbers = [1, 1, 1, 1, 1];
-const target = 3;
+const m = 6;
+const n = 6;
+const board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"];
 
-function solution(numbers, target) {
+function solution(m, n, board) {
   let answer = 0;
 
-  function dfs(num, count) {
-    if (count === numbers.length) {
-      if (num === target) {
-        answer++;
+  let gameBoard = board.map((str) => str.split(""));
+
+  let copyBoard = gameBoard.map((row) => [...row]);
+
+  let dir = [
+    [0, -1],
+    [-1, 0],
+    [-1, -1],
+  ];
+
+  // 4블록 있는 경우 0으로 바꾸어주기
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      let str = gameBoard[i][j];
+      let count = 0;
+
+      for (let z = 0; z < 3; z++) {
+        let nx = dir[z][0];
+        let ny = dir[z][1];
+
+        if (gameBoard[i + nx][j + ny] !== str) {
+          break;
+        } else if (gameBoard[i + nx][j + ny] === str) {
+          count++;
+        }
       }
 
-      return;
-    }
+      if (count === 3) {
+        answer += 4;
 
-    dfs(num + numbers[count], count + 1);
-    dfs(num - numbers[count], count + 1);
+        copyBoard[i][j] = 0;
+        for (let k = 0; k < 3; k++) {
+          let nx = dir[k][0];
+          let ny = dir[k][1];
+
+          copyBoard[i + nx][j + ny] = 0;
+        }
+      }
+    }
   }
 
-  dfs(0, 0);
-
-  return answer;
+  return copyBoard;
 }
 
-console.log(solution(numbers, target));
+console.log(solution(m, n, board));
