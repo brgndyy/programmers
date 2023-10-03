613ea63d83d4b4fb7237a428eff235ad76b060ad
brgndy <brgndy96@gmail.com>
2023-10-04 07:33:33 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,1 +1,32 @@
+const msg = `KAKAO`;
 
+function solution(msg) {
+  const answer = [];
+
+  let alphabetMap = new Map();
+
+  Array.from({ length: 26 }, (_, index) => {
+    let str = String.fromCharCode(index + 65);
+
+    alphabetMap.set(str, index + 1);
+  });
+
+  let str = "";
+
+  for (let i = 0; i < msg.length; i++) {
+    str += msg[i];
+
+    // 만약 알파벳 Map 객체가 해당 문자 + 다음 문자열을 포함하고 있지 않다면
+    if (alphabetMap.get(str) && !alphabetMap.get(str + msg[i + 1])) {
+      answer.push(alphabetMap.get(str));
+
+      alphabetMap.set(str + msg[i + 1], alphabetMap.size + 1);
+
+      str = "";
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(msg));
