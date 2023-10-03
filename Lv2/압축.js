331dedbc0cb263b373f7e7bedfb774c092a5743b
brgndy <brgndy96@gmail.com>
2023-10-04 07:33:56 +0900

    msg

--- Lv2/압축.js
+++ Lv2/압축.js
@@ -1,21 +1,32 @@
+const msg = `KAKAO`;
+
 function solution(msg) {
+  const answer = [];
+
   let alphabetMap = new Map();
-  let answer = [];
 
   Array.from({ length: 26 }, (_, index) => {
-    let char = String.fromCharCode(index + 65);
-    alphabetMap.set(char, index + 1);
+    let str = String.fromCharCode(index + 65);
+
+    alphabetMap.set(str, index + 1);
   });
 
-  let w = "";
+  let str = "";
+
   for (let i = 0; i < msg.length; i++) {
-    w += msg[i];
-    if (!alphabetMap.get(w + msg[i + 1])) {
-      answer.push(alphabetMap.get(w));
-      alphabetMap.set(w + msg[i + 1], alphabetMap.size + 1);
-      w = "";
+    str += msg[i];
+
+    // 만약 알파벳 Map 객체가 해당 문자 + 다음 문자열을 포함하고 있지 않다면
+    if (alphabetMap.get(str) && !alphabetMap.get(str + msg[i + 1])) {
+      answer.push(alphabetMap.get(str));
+
+      alphabetMap.set(str + msg[i + 1], alphabetMap.size + 1);
+
+      str = "";
     }
   }
 
   return answer;
 }
+
+console.log(solution(msg));
