747545a84527cd5364fd2d4b052a5cc1c89196e7
brgndy <brgndy96@gmail.com>
2023-10-19 07:27:16 +0900

    msg

--- debug.js
+++ debug.js
@@ -9,4 +9,4 @@ const gen = myGenerator();
 
 console.log("처음 제너레이터 실행 :", gen.next()); // Start를 출력하고, { value: 'yield 1', done: false } 반환
 console.log("제너레이터 실행 : ", gen.next()); // a: 10을 출력하고, { value: 'yield 2', done: false } 반환
-// console.log(gen.next(20));  // b: 20을 출력하고, { value: 'done', done: true } 반환
+// console.log(gen.next(20));  // b: 20을 출력하고, { value: 'done', done: true }
