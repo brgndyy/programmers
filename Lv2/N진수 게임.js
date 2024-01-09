67a62b2bbd8b63ad4b2f11effd015d25673e01c3
brgndy <brgndy96@gmail.com>
2024-01-10 07:28:31 +0900

    lv2: N진수 게임 풀이

--- Lv2/N진수 게임.js
+++ Lv2/N진수 게임.js
@@ -7,21 +7,15 @@ const m = 2;
 const p = 2;
 
 function solution(n, t, m, p) {
-  let totalStr = "";
-
-  let answer = "";
+  const totalStr = "";
 
   const 전체구해야할숫자갯수 = t * m;
 
   for (let i = 0; i <= 전체구해야할숫자갯수; i++) {
-    totalStr += i.toString(n).toUpperCase();
+    totalStr += i.toString(2).toUpperCase();
   }
 
-  for (let i = p - 1; i <= 전체구해야할숫자갯수; i += p) {
-    answer += totalStr[i];
-  }
-
-  return answer;
+  return totalStr;
 }
 
 console.log(solution(n, t, m, p));
